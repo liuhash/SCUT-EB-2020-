da4828a5342d44e2e725c738eb6c31b0cd85d78e
liuhash <89023625@qq.com>
2022-09-05 18:40:39 +0800

    last commit of ex-3 but need a test

--- ex-3/douban.py
+++ ex-3/douban.py
@@ -1,40 +1,62 @@
 import requests
-from bs4 import BeautifulSoup
+import re
+import os
+import time
+import random
+headers = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0'
+}
 
+# 不同的代理IP,代理ip的类型必须和请求url的协议头保持一致
+proxy_list = [
+     {"http": "122.9.101.6:8888"},
+    {'http': '47.113.90.161:83'}
+]
 
-def save_img(url, title):
-    res = requests.get(url, headers=headers)
-    if res.status_code == 200:
-        print('抓取成功')
-        with open('电影海报\%s.jpg' % title, 'wb') as f:  # 创建一个title.jpg的文件  '%s' %XXX
-            f.write(res.content)
-    else:
-        print('抓取失败')
+# 随机获取代理IP
+proxy = random.choice(proxy_list)
+def get_html(url):
+    '''Request a web page and get the source code'''
+    try:
 
+        html = requests.get(url, headers=headers,proxies=proxy)
+        return html.text
+    except requests.exceptions.RequestException:
+        print(url, '请求失败')
 
-def parse_data(html):
-    soup = BeautifulSoup(html, 'lxml')
-    movie_img = []
-    ol = soup.find('ol', class_='grid_view')
-    li_list = ol.find_all('li')
-    for li in li_list:
-        img = li.find('img')
-        title = img['alt']  # 把电影名字也抓取出来
-        img_url = img['src']  # 根据字典里的键取值
-        save_img(img_url, title)
+def download_image(html):
+    # print(html)
+    iter1=re.compile(r'<div class="pic">.*?<img.*?alt="(?P<movie_name>.*?)" src="(?P<img_url>.*?)".*?>.*?</div>',re.DOTALL)
+    result1=iter1.finditer(html)
+    movie_names=[]
+    img_urls=[]
+    for i in result1:
+        movie_names.append(i.group('movie_name'))
+        img_urls.append(i.group('img_url'))
+    # iter2=re.compile(r'<div class=.*?>(?P<middle>.*?)</div>',re.DOTALL)
+    # result2=iter2.finditer(html)
+    # for i in result2:
+    #     print(i.group("middle"))
+    for name,url in zip(movie_names,img_urls):  # 拿到每张图片的下载地址
+        time.sleep(1)  # 避免大规模访问   导致网站崩
+        response = requests.get(url, headers=headers,proxies=proxy)  # 这个是请求图片
+        dir_name = './pics'
+        if not os.path.exists(dir_name):
+            os.mkdir(dir_name)
+        with open(dir_name + "/" + name, "wb") as f:  # 加/是为了体现目录的替换
+            f.write(response.content)
 
+if __name__ == '__main__':
+    urls = ['https://movie.douban.com/top250?start={}&filter='.format(i * 25) for i in range(0, 10)]
+    for url in urls:
+        html = get_html(url)
+        print(html)
+        datas = download_image(html)
+    # 自己测试
+    # with open('豆瓣电影 Top 250.html') as fp:
+    #     html1=fp.read()
+    #  print(html1)
+    # download_image(html1)
 
-def get_imgs(url):  # 这里的url是页面的url
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
-    res = requests.get(url, headers=headers)
-    if res.status_code == 200:
-        html = res.text
-        parse_data(html)
-
-
-for i in range(10):
-    url = 'https://movie.douban.com/top250?start=%d&filter=' % (i * 25)
-    get_imgs(url)
 
 
--- ex-3/readme.md
+++ ex-3/readme.md
@@ -3,4 +3,11 @@ text,json,content(二进制)数据类型。
 wb binary 二进制数据。
 抓前6页的数据。豆瓣电影。
 贴海报下来。
-作业：https://blog.csdn.net/weixin_43809806/article/details/109020906
+作业：
+1.https://blog.csdn.net/weixin_43809806/article/details/109020906
+用的b4soup 不好。
+2.https://zhuanlan.zhihu.com/p/61606593
+反转了
+自己编的
+不幸的是，我的IP被封了。只能委托你们实验了。
+

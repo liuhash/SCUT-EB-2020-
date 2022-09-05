1d09446b0d96d9b3cd979db6a1c8660f8e309510
liuhash <89023625@qq.com>
2022-09-05 19:21:08 +0800

    last commit of ex-3 no need for a test

--- ex-3/douban.py
+++ ex-3/douban.py
@@ -4,22 +4,17 @@ import os
 import time
 import random
 headers = {
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0'
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0',
+    'Cookie':'写你自己的'
 }
 
 # 不同的代理IP,代理ip的类型必须和请求url的协议头保持一致
-proxy_list = [
-     {"http": "122.9.101.6:8888"},
-    {'http': '47.113.90.161:83'}
-]
+proxy_list = ['122.9.101.6','47.113.90.161','122.152.196.126','114.215.174.227','119.185.30.75']
 
-# 随机获取代理IP
-proxy = random.choice(proxy_list)
 def get_html(url):
     '''Request a web page and get the source code'''
     try:
-
-        html = requests.get(url, headers=headers,proxies=proxy)
+        html = requests.get(url, headers=headers,proxies={'http':random.choice(proxy_list)})
         return html.text
     except requests.exceptions.RequestException:
         print(url, '请求失败')
@@ -31,6 +26,8 @@ def download_image(html):
     movie_names=[]
     img_urls=[]
     for i in result1:
+        # print(i.group('movie_name'))
+        # print(i.group('img_url'))
         movie_names.append(i.group('movie_name'))
         img_urls.append(i.group('img_url'))
     # iter2=re.compile(r'<div class=.*?>(?P<middle>.*?)</div>',re.DOTALL)
@@ -39,18 +36,18 @@ def download_image(html):
     #     print(i.group("middle"))
     for name,url in zip(movie_names,img_urls):  # 拿到每张图片的下载地址
         time.sleep(1)  # 避免大规模访问   导致网站崩
-        response = requests.get(url, headers=headers,proxies=proxy)  # 这个是请求图片
+        response = requests.get(url, headers=headers,proxies={'http':random.choice(proxy_list)})  # 这个是请求图片
         dir_name = './pics'
         if not os.path.exists(dir_name):
             os.mkdir(dir_name)
-        with open(dir_name + "/" + name, "wb") as f:  # 加/是为了体现目录的替换
+        with open(dir_name + "/" + name+'.png', "wb") as f:  # 加/是为了体现目录的替换
             f.write(response.content)
 
 if __name__ == '__main__':
     urls = ['https://movie.douban.com/top250?start={}&filter='.format(i * 25) for i in range(0, 10)]
     for url in urls:
         html = get_html(url)
-        print(html)
+        # print(html)
         datas = download_image(html)
     # 自己测试
     # with open('豆瓣电影 Top 250.html') as fp:
--- ex-3/readme.md
+++ ex-3/readme.md
@@ -4,10 +4,14 @@ wb binary 二进制数据。
 抓前6页的数据。豆瓣电影。
 贴海报下来。
 作业：
+参考文献:
 1.https://blog.csdn.net/weixin_43809806/article/details/109020906
 用的b4soup 不好。
 2.https://zhuanlan.zhihu.com/p/61606593
 反转了
 自己编的
 不幸的是，我的IP被封了。只能委托你们实验了。
-
+反转了
+我通过携带cookie的方式解决了。跑一次大概5min左右。
+可以自己跑一下。cookie的部分用你自己的。
+怎么拿到cookie自己搜。
